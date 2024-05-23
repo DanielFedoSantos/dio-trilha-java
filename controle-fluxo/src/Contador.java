723a9de3a9174d9fc5d55c34020f08cc4924d12c
DanielFedoSantos <danielfedosantos@gmail.com>
2024-05-23 07:20:04 -0300

    fix contador

--- controle-fluxo/src/Contador.java
+++ controle-fluxo/src/Contador.java
@@ -7,6 +7,8 @@ public class Contador {
 		int parametroUm = terminal.nextInt();
 		System.out.println("Digite o segundo parâmetro");
 		int parametroDois = terminal.nextInt();
+
+		terminal.close();
 		
 		try {
 			contar(parametroUm, parametroDois);
@@ -20,13 +22,11 @@ public class Contador {
 
         if(parametroUm >= parametroDois)
             throw new ParametrosInvalidosException();
-		//validar se parametroUm é MAIOR que parametroDois e lançar a exceção
 		
 		int contagem = parametroDois - parametroUm;
 
 		for(int i = 1; i <= contagem; i++) {
 			System.out.println("Imprimindo o numero " + i);
 		}
-		//realizar o for para imprimir os números com base na variável contagem
 	}
 }
